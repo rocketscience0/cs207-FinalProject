43c791591695671e3260786fa1bb52542617b514
Chris Siviy <csiviy@g.harvard.edu>
2019-11-08 13:11:29 -0500

    Expanded docstring for @elementary

--- autodiff/operations.py
+++ autodiff/operations.py
@@ -6,6 +6,33 @@ import numpy as np
 
 
 def elementary(deriv_func):
+    """Decorator to create an elementary operation
+
+    This takes as an argument a function that calculates the derivative of the function
+    the user is calculating. When the decorated function is called, `@elementary` also calls
+    `deriv_func` and stores both the value and derivative in a new `Number()` object
+
+    Example:
+        >>> import numpy as np
+        >>> def sin_deriv(x):
+        ...     return {x: np.cos(x.val) * x.deriv[x]}
+        >>> @elementary(sin_deriv)
+        ... def sin(x):
+        ...     return np.sin(x.val)
+        >>> a = Number(np.pi / 2)
+        >>> sina = sin(a)
+        >>> sina.val
+        1.0
+        >>> sina.deriv[a]
+        0.0
+
+    
+    Args:
+        deriv_func (function): Function specifying the derivative of this function. Must return a dictionary where each key-value pair is the partial derivative of the decorated function
+    
+    Returns:
+        function: Decorated function
+    """
     def inner(func):
         print('Inside wrap()')
         def inner_func(*args):
