0f0282437b3de3ed5de1e9ec677d5bc2dad99fcd
Allison Kao <allisonkao@dhcp-10-250-27-44.harvard.edu>
2019-12-07 15:29:42 -0500

    Add array implementation without matrix multiplication

--- autodiff/structures.py
+++ autodiff/structures.py
@@ -343,4 +343,422 @@ class Number():
             return False
         except Exception:
             raise Exception('cannot compare autodiff.Number object with a non-Number object')
+
+
+class ArrayIterator:
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self.index = 0
+
+    def __next__(self):
+        try:
+            number = self.numbers[self.index]
+        except IndexError:
+            raise StopIteration()
+        self.index += 1 
+        return number
+
+    def __iter__(self):
+        return self
+
+
+class Array:
+
+    def __init__(self, iterable):
+        self._lst = []
+        flat_iterable = np.array(iterable).flatten()
+        for elt in flat_iterable:
+            # Check if element is number type
+            if not isinstance(elt, Number):
+                number_elt = Number(elt)
+            else:
+                number_elt = elt
+            self._lst.append(number_elt)
+        self._lst = np.array(self._lst).reshape(np.shape(iterable))
+
+    def __len__(self):
+        return len(self._lst)
+
+    def __repr__(self):
+        return f'Array({self._lst})'
+
+    def __getitem__(self, idx):
+        return self._lst[idx]
+
+    def __setitem__(self, idx, val):
+        self.__setattr__(idx, val)
+
+    def __delitem__(self, idx):
+        self.__delattr__(idx)
+
+    def __iter__(self):
+        return ArrayIterator(self._lst)
+
+    # Element-wise addition of 2 vectors or 1 vector, 1 scalar
+    def add(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.add(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.add(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.add(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Concatenate
+    def __add__(self, other):
+        if isinstance(other, Array):
+            out = np.append(self._lst,other._lst)
+        else:
+            try: 
+                is_iterable = iter(other)
+            except:
+                raise TypeError("can only concatenate iterable s(not {})".format(type(other)))
+        return Array(out)
     
+    def __radd__(self, other):
+        if isinstance(other, Array):
+            out = np.append(self._lst,other._lst)
+        else:
+            try: 
+                is_iterable = iter(other)
+            except:
+                raise TypeError("can only concatenate iterable s(not {})".format(type(other)))
+        return Array(out)
+
+    # Element-wise subtraction of 2 vectors or 1 vector, 1 scalar
+    def subtract(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.subtract(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.subtract(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.subtract(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Element-wise subtraction of 2 vectors or 1 vector, 1 scalar
+    def __sub__(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.subtract(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.subtract(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.subtract(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+    
+    def __rsub__(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.subtract(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.subtract(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.subtract(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Scalar multiplication or element-wise multiplication
+    def __mul__(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.mul(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.mul(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.mul(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    def __rmul__(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.mul(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.mul(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.mul(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Element-wise multiplication, Numpy-specific
+    def multiply(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.mul(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                raise TypeError("{} object is not subscriptable".format(type(other)))
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.mul(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Can only separate 2D arrays
+    # TODO: NOT WORKING RN
+    # def __matmul__(self, other):
+    #     out = None
+    #     shape = None
+    #     if isinstance(other, Array):
+    #         if np.shape(self._lst)[1] == np.shape(other._lst)[0]:
+    #             shape = (np.shape(self._lst)[0], np.shape(other._lst)[1])
+    #             out = np.full(shape,Number(0))
+    #             for row_idx in range(shape[0]):
+    #                 for col_idx in range(shape[1]):
+    #                     new = Number(0)
+    #                     for prod_idx in range(np.shape(self._lst)[1]):
+    #                         prod = operations.mul(self._lst[row_idx, prod_idx], other._lst[prod_idx, col_idx])
+    #                         new = operations.add(new, prod)
+    #                     out[row_idx][col_idx] = new
+    #         else:
+    #             raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+    #     else:
+    #         try:
+    #             is_iterable = iter(other)
+    #         except:
+    #             raise TypeError("{} object is not subscriptable".format(type(other)))
+    #         if np.shape(self._lst)[1] == np.shape(other)[0]:
+    #             shape = (np.shape(self._lst)[0], np.shape(other)[1])
+    #             out = np.array(np.shape(shape))
+    #             for row_idx in range(shape[0]):
+    #                 for col_idx in range(shape[1]):
+    #                     new = Number(0)
+    #                     for prod_idx in range(np.shape(self._lst)[1]):
+    #                         prod = operations.mul(self._lst[row_idx, prod_idx], other[prod_idx, col_idx])
+    #                         new = operations.add(new, prod)
+    #                     out[row_idx, col_idx] = new
+    #         else:
+    #             raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+    #     return Array(out)  
+
+    # def __rmatmul__(self, other):     
+
+    # Element-wise division, Numpy-specific
+    def divide(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.div(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                raise TypeError("{} object is not subscriptable".format(type(other)))
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.div(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Scalar or element-wise division
+    def __truediv__(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.div(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.div(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.div(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    def __rtruediv__(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.div(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.div(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.div(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Scalar or element-wise power, like Numpy
+    def power(self, other):
+        out = []
+        if isinstance(other, Array):
+            if np.shape(self._lst) == np.shape(other._lst):
+                for self_elt,other_elt in zip(self._lst.flatten(), other._lst.flatten()):
+                    out.append(operations.power(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other._lst)))
+        else:
+            try:
+                is_iterable = iter(other)
+            except:
+                for elt in self._lst:
+                    out.append(operations.power(elt,other))
+                return Array(out)
+            if np.shape(self._lst) == np.shape(other):
+                other_flattened = np.array(other).flatten()
+                for self_elt,other_elt in zip(self._lst.flatten(), other_flattened):
+                    out.append(operations.power(self_elt,other_elt))
+            else:
+                raise ValueError("operands could not be broadcast together with shapes {} and {}".format(np.shape(self._lst),np.shape(other)))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    def __pow__(self, other):
+        raise TypeError("unsupported operand type(s) for ** or pow(): {} and {}".format(type(self), type(other)))
+
+    def __rpow__(self, other):
+        raise TypeError("unsupported operand type(s) for ** or pow(): {} and {}".format(type(self), type(other)))
+
+    def __neg__(self):
+        out = []
+        flat_iterable = self._lst.flatten()
+        for elt in flat_iterable:
+            out.append(operations.negate(elt))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    def sin(self):
+        out = []
+        flat_iterable = self._lst.flatten()
+        for elt in flat_iterable:
+            out.append(operations.sin(elt))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+    
+    def cos(self):
+        out = []
+        flat_iterable = self._lst.flatten()
+        for elt in flat_iterable:
+            out.append(operations.cos(elt))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+    
+    def tan(self):
+        out = []
+        flat_iterable = self._lst.flatten()
+        for elt in flat_iterable:
+            out.append(operations.tan(elt))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    def exp(self):
+        out = []
+        flat_iterable = self._lst.flatten()
+        for elt in flat_iterable:
+            out.append(operations.exp(elt))
+        return Array(np.array(out).reshape(np.shape(self._lst)))
+
+    # Summing across all elements in a vector, regardless of axis
+    def sum(self):
+        out = np.sum(self._lst).val
+        return Number(out)
+
+    # Dot product
+    def dot(self, other):
+        product = self.multiply(other)
+        out = product.sum()
+        return out
