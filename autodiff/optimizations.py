abc54c02cea807367028045f7bada34e1c285ce4
Shijian Jin <sjin@g.harvard.edu>
2019-12-08 13:01:31 -0500

    update vector bfgs (haven't tested due to issue with Number/Array operations; algorithm should be correct), and vector gradient_descent method)

--- autodiff/optimizations.py
+++ autodiff/optimizations.py
@@ -17,54 +17,116 @@ def bfgs(func, initial_guess,iterations =100):
         x0: the x value of the local extremum
         func(x0): the value of the local extremum
         jacobians: the jacobians of each optimization step
-        """    
+        """   
+    if isinstance(initial_guess,Number): 
     #bfgs for scalar functions
     
-    x0 = initial_guess
-    
-    #initial guess of hessian
-    b0 = 1
-    
-    fxn0 = func(x0)
-
-    fpxn0 = fxn0.jacobian(x0)
-    
-    jacobians = []
-    
-    jacobians.append(fpxn0)
-    
-    while(np.abs(fpxn0)>1*10**-7):
+        x0 = initial_guess
+        
+        #initial guess of hessian
+        b0 = 1
+        
         fxn0 = func(x0)
 
         fpxn0 = fxn0.jacobian(x0)
+        
+        jacobians = []
+        
+        jacobians.append(fpxn0)
+        
 
-        s0 = -fpxn0/b0
+        for i in range(iterations):
+            # need a stopping criterion
+            fxn0 = func(x0)
 
-        x1=x0+s0 
-        
-        fxn1 = func(x1)
-        fpxn1 = fxn1.jacobian(x1)
-        
-        
-        y0 = fpxn1-fpxn0
-        
-        if y0 == 0:
-            break
+            fpxn0 = fxn0.jacobian(x0)
+
+            s0 = -fpxn0/b0
+
+            x1=x0+s0 
+                
+            fxn1 = func(x1)
+            fpxn1 = fxn1.jacobian(x1)
+                
+                
+            y0 = fpxn1-fpxn0
+                
+            if y0 == 0:
+                break
+                    
+                #delta_b = y0**2/(y0*s0)-b0*s0**2*b0/(s0*b0*s0)
+            delta_b = y0/s0-b0
+            b1 = b0 + delta_b
+                
+            x0 = x1
+                
+            b0 = b1
+                
+            jacobians.append(fpxn1)
+
+        return x0,func(x0),jacobians
+
+
+        if isinstance(initial_guess,Array):
+
+        #Vector case of BFGS optimization 
+
+            x0 = initial_guess
+
+            #initial guess of hessian
+            h0 = np.identity(len(x0))
+
+            fxn0 = func(x0)
+
+            fpxn0 = fxn0.jacobian(x0)
             
-        #delta_b = y0**2/(y0*s0)-b0*s0**2*b0/(s0*b0*s0)
-        delta_b = y0/s0-b0
-        b1 = b0 + delta_b
-        
-        x0 = x1
-        
-        b0 = b1
-        
-        jacobians.append(fpxn1)
+            jacobians = []
 
-    return x0,func(x0),jacobians
+            jacobians.append(fpxn0)
+
+            s0 = -np.dot(h0,fpxn0) #np.array multiply with scalar would be fine
+            
+            x1 = x0 + s0
+            
+            fxn1 = func(x1)
+            
+            fpxn1 = fxn1.jacobian(x1)
+            
+            y0 = np.array(fpxn1 - fpxn0)
+            
+            rho = 1/(y0.T,s0)
+            intermediate_1 = np.identity(len(x0))-np.dot(np.dot(s0,rho0),y0.T)
+            intermediate_2 = np.dot(np.dot(intermediate1,h0),intermediate1)
+            intermediate_3 = np.dot(np.dot(rho0,s0),s0.T)
+            
+            delta_H = intermediate_2 + intermediate_3
+            
+            
+            for i in range(iterations):
+                #need a stopping criterion
+                jacobians.append(fpxn1)
+
+                h0 = h0 + delta_H
+                x0 = x1 
+                fxn0 = func(x0)
+                fpxn0 = fxn0.jacobian(x0)
+                
+                s0 = -np.dot(h0,fpxn0) #np.array multiply with scalar would be fine
+                x1 = x0 + s0
+                fxn1 = func(x1)
+                fpxn1 = fxn1.jacobian(x1)
+                y0 = np.array(fpxn1 - fpxn0)
+                
+                rho = 1/(y0.T,s0)
+                intermediate_1 = np.identity(len(x0))-np.dot(np.dot(s0,rho0),y0.T)
+                intermediate_2 = np.dot(np.dot(intermediate1,h0),intermediate1)
+                intermediate_3 = np.dot(np.dot(rho0,s0),s0.T)
+                delta_H = intermediate_2 + intermediate_3
+                
+            return x0,func(x0),jacobians
   
 
-def steepest_descent(func,initial_guess,iterations = 100,step_size=0.01):
+def gradient_descent(func,initial_guess,iterations = 100,step_size=0.01):
 
     """Use steepest_descent method to find the local minimum/maxinum of the function
     Args:
@@ -78,20 +140,62 @@ def steepest_descent(func,initial_guess,iterations = 100,step_size=0.01):
         func(x0): the value of the local extremum
         jacobians: the jacobians of each optimization step
         """    
-    #bfgs for scalar functions
+    #gradient descent for scalar functions
+    if isinstance(initial_guess,Number):
+        x0=initial_guess
+        jacobians = []
+        s = -func(initial_guess).jacobian(x0)
+        jacobians.append(s)
+        for i in range(iterations):
+            if np.abs(s)>1*10**-7:
+                x0 = x0 + step_size*s
+                s = -func(x0).jacobian(x0)
+                jacobians.append(s)
+        
+        
+        return x0,func(x0),jacobians
+    elif isinstance(initial_guess,Array):
+        #operations are different due to different properties of jacobians for Number and Array
 
-    x0=initial_guess
-    jacobians = []
-    s = -func(initial_guess).jacobian(x0)
-    jacobians.append(s)
-    for i in range(iterations):
-        if np.abs(s)>1*10**-7:
-            x0 = x0 + step_size*s
-            s = -func(x0).jacobian(x0)
+        # e.g. R2 --> R3
+        if isinstance(func(initial_guess),tuple):
+            x0=initial_guess
+            jacobians = []
+            s = -Array(func(initial_guess)).jacobian(x0)
             jacobians.append(s)
-    
-    
-    return x0,func(x0),jacobians
+
+            for i in range(iterations):
+
+                #if np.abs(s.sum())>10**-7:
+                for j in range(len(s)):
+                    x0 = np.array(x0).reshape(-1,1) + np.array(step_size*s[j]).reshape(-1,1)
+
+                    x0 = Array(x0.reshape(len(x0)))
+
+                s = -Array(func(x0)).jacobian(x0)
+
+                jacobians.append(s)
+            return x0,func(x0),jacobians
+
+        # e.g. R2 --> R1
+        elif isinstance(func(initial_guess),Number):
+            x0=initial_guess
+            jacobians = []
+            s = -np.array(func(initial_guess).jacobian(x0))#negative sign cannot be used on a list, so turn into a np array
+            jacobians.append(s)
+
+            for i in range(iterations):
+
+                #if np.abs(s.sum())>10**-7:
+                for j in range(len(s)):
+                    x0 = np.array(x0).reshape(-1,1) + np.array(step_size*s[j]).reshape(-1,1)
+
+                    x0 = Array(x0.reshape(len(x0)))
+                print(i,x0)
+                s = -np.array(func(x0).jacobian(x0))
+
+                jacobians.append(s)
+            return x0,func(x0),jacobians
 
         
         
